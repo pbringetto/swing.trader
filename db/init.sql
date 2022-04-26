abec7e316d62d41cb0357799404ba7022e83b98a
pbringetto <plbriqto@gmail.com>
2022-04-26 11:53:29 -0700

    reset

--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE `market_state` (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     pair VARCHAR(8),
     price  FLOAT(8),
-    `type` VARCHAR(8),
+    `type` VARCHAR(16),
     time_frame INT,
     primary key (id)
 );
@@ -52,7 +52,9 @@ CREATE TABLE `settings` (
     primary key (id)
 );
 
-INSERT INTO `market_state` VALUES (2,'2022-04-21 03:57:03','XBTUSDT',40234,'oversold',15);
-INSERT INTO `market_state` VALUES (1,'2022-04-15 03:57:03','XBTUSDT',39622,'oversold',1440);
-INSERT INTO `settings` (setting, enabled) VALUES ('trading_enabled', 0);
+(1,'2022-01-15 03:57:03','XBTUSDT',34622,'oversold',1440),
+(2,'2022-04-21 03:57:03','XBTUSDT',40234,'oversold',15),
+(3,'2022-04-25 23:33:05','XBTUSDT',40520.5,'overbought',15);
+(4,'2022-01-22 23:33:05','XBTUSDT',35522,'oversold',240);
+INSERT INTO `settings` (setting, enabled) VALUES ('trading_enabled', 1);
 INSERT INTO `settings` (setting, enabled) VALUES ('created_at', 1);
