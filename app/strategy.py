62326bc941de093793c11a5fb924d7915cd68509
pbringetto <plbriqto@gmail.com>
2022-06-04 08:45:11 -0700

    strategy

--- app/strategy.py
+++ app/strategy.py
@@ -2,26 +2,31 @@ import ta_signals
 import pyangles
 
 class Strategy:
-    def signal_keys(self):
-        return (
-            ['macd_rising', 'macd_over_centerline', 'macd_over_signal', 'bollinger_at_matp'],
-            ['macd_dropping', 'macd_under_centerline', 'macd_under_signal', 'bollinger_below_matp']
-        )
-
     def setup(self, ohlc, time_frame, pair):
         price = float(ohlc['close'][::-1][0])
-        buy = 0
-        sell = 0
+        buy = []
+        sell = []
 
-        buy_signals, sell_signals = self.signal_keys()
         ta_data, ohlc = ta_signals.go(ohlc, 'close')
 
         signals = []
         for signal in ta_data:
             if signal['value']:
+                print(signal['value'])
                 signals.append(signal['key'])
 
-        buy =  all(item in signals for item in buy_signals)
-        sell = all(item in signals for item in sell_signals)
-        return buy, sell
+        print('---------ta_data--------------------')
+        print(signals)
+        print('---------strategy_data--------------------')
+        print(time_frame['buy_signals'])
+        print(time_frame['sell_signals'])
+        print('------------------------------------')
+
+        for signal_group in time_frame['buy_signals']:
+            buy.append(all(item in signals for item in signal_group))
+
+        for signal_group in time_frame['sell_signals']:
+            sell.append(all(item in signals for item in signal_group))
+
+        return True in buy, True in sell
 
